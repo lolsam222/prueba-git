7e70654d6536cde1ed55d85770df2e04bb3e83bc
unknown <svenegasb@unal.edu.co>
2026-05-07 16:04:16 -0500

    se agregó una copia del archivo gti_ccp.cpp pero con otro nombre(correción de ccp a cpp

---
+++ nuevo_git.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"hola"<<endl;
+	int i=0;
+	cout<<"pon un numero: ";
+	cin>>i;
+	for(int j =0; j<i; j++) cout<<j<<" x "<<i<<" = "<<j*i<<endl;
+	
+}
