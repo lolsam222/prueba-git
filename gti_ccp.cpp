8bdce736624a8dc8cde2d909e3e559d1fd70fa03
unknown <svenegasb@unal.edu.co>
2026-05-07 15:59:17 -0500

    se edito el archivo gti_ccp.ccp

--- gti_ccp.cpp
+++ gti_ccp.cpp
@@ -3,5 +3,10 @@
 using namespace std;
 
 int main(){
-	cout<<"hola";
+	cout<<"hola"<<endl;
+	int i=0;
+	cout<<"pon un numero: ";
+	cin>>i;
+	for(int j =0; j<i; j++) cout<<j<<" x "<<i<<" = "<<j*i<<endl;
+	
 }
